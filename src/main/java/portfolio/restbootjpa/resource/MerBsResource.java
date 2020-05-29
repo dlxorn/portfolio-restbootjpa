1b031c8489aa1d85da24acfbb0e98577eb198b74
LeeTeaGu <likebat@naver.com>
2020-05-29 21:33:46 +0900

    수정

---
+++ src/main/java/portfolio/restbootjpa/resource/MerBsResource.java
@@ -0,0 +1,44 @@
+package portfolio.restbootjpa.resource;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+
+import portfolio.restbootjpa.controller.MerBsController;
+import portfolio.restbootjpa.dto.MerBsDto;
+@Relation(collectionRelation = "merbsinfolist")                                   
+public class MerBsResource  extends  RepresentationModel<MerBsResource>{
+
+	@JsonUnwrapped
+	private MerBsDto merBsDto;
+	
+	
+	public MerBsResource( MerBsDto merBsDto, boolean isLogin) {
+	    this();
+		this.merBsDto = merBsDto;		
+		this.add(linkTo(MerBsController.class).slash(merBsDto.getMerNo()).withRel("read-merbs").withType(RequestMethod.GET.name()));			
+				
+		if(isLogin) {
+		this.add(linkTo(MerBsController.class).withRel("create-merbs").withType(RequestMethod.POST.name()));		
+		this.add(linkTo(MerBsController.class).withRel("update-merbs").withType(RequestMethod.PUT.name()));
+		this.add(linkTo(MerBsController.class).slash(merBsDto.getMerNo()).withRel("delete-merbs").withType(RequestMethod.DELETE.name()));
+		}
+		
+	}
+	
+	
+	public MerBsResource() {	
+		
+		this.add(linkTo(MerBsController.class).withRel("merbs-list").withType(RequestMethod.GET.name()));
+		
+		
+	}
+		
+			
+
+}
