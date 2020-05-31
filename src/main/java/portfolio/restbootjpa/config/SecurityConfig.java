be84692067f1a31f102d59e10e4261db3c65840c
LeeTeaGu <likebat@naver.com>
2020-05-31 17:41:25 +0900

    소셜 로그인 설정 중(작동안함)

--- src/main/java/portfolio/restbootjpa/config/SecurityConfig.java
+++ src/main/java/portfolio/restbootjpa/config/SecurityConfig.java
@@ -1,15 +1,42 @@
 package portfolio.restbootjpa.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+import portfolio.restbootjpa.accounts.AccountService;
+import portfolio.restbootjpa.accounts.CustomOAuth2UserService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	AccountService accountService;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	CustomOAuth2UserService customOAuth2UserService;
+	
+	
+	
+	@Bean
+	public TokenStore tokenStore() {
+		return new InMemoryTokenStore();		
+	}
+		
 	
 	
 	protected void configure(HttpSecurity http) throws Exception {	
@@ -18,7 +45,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 				.anyRequest().authenticated()
 				.and()
 			.formLogin().and()
-			.httpBasic();
+			.httpBasic()
+			.and()
+				.oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
 	}
 	
 	
@@ -27,6 +56,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 	    return super.authenticationManagerBean();
 	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(accountService)
+			.passwordEncoder(passwordEncoder) ;
+	}
+		
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().mvcMatchers("/docs/index.html"); 
+		web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());				
+
+	}
+	
+
 	
 	
 
